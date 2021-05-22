5dd81f542146ea010a54b882a59dc5b65e698e72
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2021-05-22 17:34:11 -0400

    Projeto Criado com Java 11 Spring Boot

---
+++ src/main/java/com/escolaon/ucan/UCan1Application.java
@@ -0,0 +1,13 @@
+package com.escolaon.ucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UCan1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/escolaon/ucan/UCan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escolaon.ucan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UCan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
